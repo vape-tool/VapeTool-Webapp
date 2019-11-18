b0dbb2b877a8736f2408d8aa4b9bd4da8bb90490
Stanislaw Baranski <stachu@stasbar.com>
2019-11-18 13:38:46 +0100

    center wizzard window

--- src/pages/user/wizard/index.tsx
+++ src/pages/user/wizard/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Input, Spin } from 'antd';
+import { Button, Card, Input, Spin, Row, Col } from 'antd';
 import React from 'react';
 import { connect } from 'dva';
 import ButtonGroup from 'antd/es/button/button-group';
@@ -31,46 +31,52 @@ const UserWizard: React.FC<{
   }
   return (
     <div>
-      <Card style={{ maxWidth: 500 }} title="Setup user">
-        <div className={styles.avatarHolder}>
-          <div onClick={showNewAvatarChooser}>
-            <div>
-              {newAvatarUrl && <img alt="avatar" src={newAvatarUrl} width={200} />}
-              {!newAvatarUrl && (
-                <FirebaseImage type="user" id={currentUser.uid} size={200} shape="square" />
-              )}
+      <Row type="flex">
+        <Col xs={0} md={4} lg={6} xl={8} />
+        <Col xs={24} md={16} lg={14} xl={10}>
+          <Card style={{ maxWidth: 500 }} title="Setup user">
+            <div className={styles.avatarHolder}>
+              <div onClick={showNewAvatarChooser}>
+                <div>
+                  {newAvatarUrl && <img alt="avatar" src={newAvatarUrl} width={200} />}
+                  {!newAvatarUrl && (
+                    <FirebaseImage type="user" id={currentUser.uid} size={200} shape="square" />
+                  )}
+                </div>
+              </div>
+              <br />
+              <div className={styles.name}>
+                <Input
+                  style={{
+                    textAlign: 'center',
+                    display: 'block',
+                    outline: 0,
+                    wordWrap: 'break-word',
+                    boxSizing: 'inherit',
+                    cursor: 'text',
+                    minHeight: 50,
+                    lineHeight: '37px',
+                    fontSize: 28,
+                    fontFamily: 'Proxima Nova Bold,Helvetica Neue,Helvetica,Arial,sans-serif',
+                  }}
+                  placeholder={currentUser.name}
+                  onChange={onDisplayNameChange}
+                  value={displayName || currentUser.name}
+                />
+              </div>
             </div>
-          </div>
-          <br />
-          <div className={styles.name}>
-            <Input
-              style={{
-                textAlign: 'center',
-                display: 'block',
-                outline: 0,
-                wordWrap: 'break-word',
-                boxSizing: 'inherit',
-                cursor: 'text',
-                minHeight: 50,
-                lineHeight: '37px',
-                fontSize: 28,
-                fontFamily: 'Proxima Nova Bold,Helvetica Neue,Helvetica,Arial,sans-serif',
-              }}
-              placeholder={currentUser.name}
-              onChange={onDisplayNameChange}
-              value={displayName || currentUser.name}
-            />
-          </div>
-        </div>
-        <div style={{ textAlign: 'right' }}>
-          <ButtonGroup>
-            <Button onClick={onCancel}>Cancel</Button>
-            <Button icon="save" type="primary" onClick={onSave}>
-              Save
-            </Button>
-          </ButtonGroup>
-        </div>
-      </Card>
+            <div style={{ textAlign: 'right' }}>
+              <ButtonGroup>
+                <Button onClick={onCancel}>Cancel</Button>
+                <Button icon="save" type="primary" onClick={onSave}>
+                  Save
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Card>
+        </Col>
+        <Col xs={0} md={4} lg={6} xl={8} />
+      </Row>
       <ImageChooser
         uploadHintText="Upload avatar photo. Make sure that the photo doesn't brake the rules."
         visible={showAvatarChooser}
