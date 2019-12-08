8806cc7b979586adcb973778a1767c64384853b7
stasbar <stasbar1995@gmail.com>
2019-12-09 00:48:11 +0100

    fix UploadPhoto width
    Took 49 seconds

--- src/pages/cloud/UploadPhoto.tsx
+++ src/pages/cloud/UploadPhoto.tsx
@@ -38,83 +38,77 @@ const UploadPhoto: React.FC<UploadPhotoProps> = props => {
 
   return (
     <div className="App">
-      <Row type="flex">
-        <Col xs={0} md={4} lg={6} xl={8} />
-        <Col xs={24} md={16} lg={14} xl={10}>
-          {croppedImageUrl && (
-            <Input
-              style={{
-                display: 'block',
-                outline: 0,
-                wordWrap: 'break-word',
-                boxSizing: 'inherit',
-                cursor: 'text',
-                minHeight: 50,
-                lineHeight: '37px',
-                fontSize: 28,
-                fontFamily: 'Proxima Nova Bold,Helvetica Neue,Helvetica,Arial,sans-serif',
-                border: 0,
-              }}
-              placeholder="Say something about this photo"
-              onChange={onDescriptionChange}
-              value={description}
-            />
-          )}
-          {!croppedImageUrl && (
-            <div
-              style={{
-                display: 'table',
-                float: 'left',
-                verticalAlign: 'top',
-                width: '100%',
-                height: 300,
-                backgroundColor: '#fafafa',
-                cursor: 'pointer',
-                textAlign: 'center',
-                border: '1px dashed #d9d9d9',
-                borderRadius: 4,
-                boxSizing: 'border-box',
-                fontSize: 30,
-              }}
-              onClick={showNewPhotoChooser}
-            >
-              <div
-                style={{
-                  textAlign: 'center',
-                  display: 'table-cell',
-                  width: '100%',
-                  height: '100%',
-                  verticalAlign: 'middle',
-                }}
-              >
-                <Icon type="plus" style={{ color: '#999' }} />
-                <div className="ant-upload-text" style={{ color: '#666' }}>
-                  Click to upload photo
-                </div>
-              </div>
+      {croppedImageUrl && (
+        <Input
+          style={{
+            display: 'block',
+            outline: 0,
+            wordWrap: 'break-word',
+            boxSizing: 'inherit',
+            cursor: 'text',
+            minHeight: 50,
+            lineHeight: '37px',
+            fontSize: 28,
+            fontFamily: 'Proxima Nova Bold,Helvetica Neue,Helvetica,Arial,sans-serif',
+            border: 0,
+          }}
+          placeholder="Say something about this photo"
+          onChange={onDescriptionChange}
+          value={description}
+        />
+      )}
+      {!croppedImageUrl && (
+        <div
+          style={{
+            display: 'table',
+            float: 'left',
+            verticalAlign: 'top',
+            width: '100%',
+            height: 300,
+            backgroundColor: '#fafafa',
+            cursor: 'pointer',
+            textAlign: 'center',
+            border: '1px dashed #d9d9d9',
+            borderRadius: 4,
+            boxSizing: 'border-box',
+            fontSize: 30,
+          }}
+          onClick={showNewPhotoChooser}
+        >
+          <div
+            style={{
+              textAlign: 'center',
+              display: 'table-cell',
+              width: '100%',
+              height: '100%',
+              verticalAlign: 'middle',
+            }}
+          >
+            <Icon type="plus" style={{ color: '#999' }} />
+            <div className="ant-upload-text" style={{ color: '#666' }}>
+              Click to upload photo
             </div>
-          )}
-          {croppedImageUrl && (
-            <img
-              alt="Crop"
-              width="100%"
-              style={{ maxWidth: '100%' }}
-              src={croppedImageUrl}
-              onClick={showNewPhotoChooser}
-            />
-          )}
-          {croppedImageUrl && (
-            <Row>
-              <Col xs={24}>
-                <Button type="primary" size="large" block onClick={postPhoto}>
-                  Post
-                </Button>
-              </Col>
-            </Row>
-          )}
-        </Col>
-        <Col xs={0} md={4} lg={6} xl={8} />
-      </Row>
+          </div>
+        </div>
+      )}
+      {croppedImageUrl && (
+        <img
+          alt="Crop"
+          width="100%"
+          style={{ maxWidth: '100%' }}
+          src={croppedImageUrl}
+          onClick={showNewPhotoChooser}
+        />
+      )}
+      {croppedImageUrl && (
+        <Row>
+          <Col xs={24}>
+            <Button type="primary" size="large" block onClick={postPhoto}>
+              Post
+            </Button>
+          </Col>
+        </Row>
+      )}
       <ImageChooser
         visible={showPhotoChooser === true || (!cancelled && !croppedImageUrl)}
         onCancel={hideNewPhotoChooser}
