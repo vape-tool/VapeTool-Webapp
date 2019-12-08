9245c63839b1c8152afdc0da6b2188daac21701b
stasbar <stasbar1995@gmail.com>
2019-12-09 00:14:53 +0100

    CommentIconText and LikeIconText click works on icon and text
    Took 6 hours 53 minutes

--- src/components/CommentIconText/index.tsx
+++ src/components/CommentIconText/index.tsx
@@ -1,18 +1,17 @@
 import { Icon } from 'antd';
 import React from 'react';
 
-export const CommentIconText = (
-  {
-    type,
-    text,
-    onClick,
-  }: {
-    type: string;
-    text: string;
-    onClick: any;
-  }) => (
-  <span>
-        <Icon onClick={onClick} type={type} style={{ marginRight: 8 }}/>
+export const CommentIconText = ({
+  type,
+  text,
+  onClick,
+}: {
+  type: string;
+  text: string;
+  onClick: any;
+}) => (
+  <span onClick={onClick}>
+    <Icon type={type} style={{ marginRight: 8 }} />
     {text}
-      </span>
+  </span>
 );
--- src/components/LikeIconText/index.tsx
+++ src/components/LikeIconText/index.tsx
@@ -2,26 +2,24 @@ import { Icon } from 'antd';
 import React from 'react';
 import styles from '@/components/PhotoView/index.less';
 
-export const LikeIconText =
-  ({
-     type,
-     text,
-     onClick,
-     likedByMe,
-   }: {
-    type: string;
-    text: string;
-    onClick: any;
-    likedByMe?: boolean;
-  }) => (
-    <span>
-        <Icon
-          onClick={onClick}
-          type={type}
-          theme={likedByMe ? 'filled' : 'outlined'}
-          className={likedByMe ? styles.liked : ''}
-          style={{ marginRight: 8 }}
-        />
-      {text}
-      </span>
-  );
+export const LikeIconText = ({
+  type,
+  text,
+  onClick,
+  likedByMe,
+}: {
+  type: string;
+  text: string;
+  onClick: any;
+  likedByMe?: boolean;
+}) => (
+  <span onClick={onClick}>
+    <Icon
+      type={type}
+      theme={likedByMe ? 'filled' : 'outlined'}
+      className={likedByMe ? styles.liked : ''}
+      style={{ marginRight: 8 }}
+    />
+    {text}
+  </span>
+);
