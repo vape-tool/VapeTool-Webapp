b3a35b9e78ecb7b4c560b4005b8b090b384d06db
stasbar <stasbar1995@gmail.com>
2019-12-11 00:07:51 +0100

    inject APP_TYPE
    Took 17 minutes

--- config/config.ts
+++ config/config.ts
@@ -231,6 +231,7 @@ export default {
   define: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
       ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+    'process.env.APP_TYPE': process.env.APP_TYPE || '',
   },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
