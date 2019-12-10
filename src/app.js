30bc60b25836803e58e7fa7264114ed296119e0d
stasbar <stasbar1995@gmail.com>
2019-12-11 00:16:23 +0100

    Took 10 minutes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { persistEnhancer } from 'dva-model-persist';
 import storage from 'dva-model-persist/lib/storage/session';
-/* eslint-disable */
+
 export const dva = {
   config: {
     onError(e) {
@@ -25,6 +25,6 @@ export const dva = {
       }),
     ],
   },
-  plugins: [process.env.APP_TYPE === 'build' ? null : require('dva-logger')()],
+  // eslint-disable-next-line global-require
+  plugins: [process.env.APP_TYPE === 'build' ? {} : require('dva-logger')()],
 };
-/* eslint-enable */
