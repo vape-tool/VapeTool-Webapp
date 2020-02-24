3e3d41a362fad3abadac8ed0f249d2ba7d96192e
Argeath <kinaldominik@gmail.com>
2020-02-24 20:21:06 +0100

    Payment page
    Took 2 hours 36 minutes

--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -1,86 +1,133 @@
 import * as React from 'react';
-import { Button, Card, Col, Row, Typography } from 'antd';
+import { Button, Card, Col, Icon, Radio, Row, Tag, Typography } from 'antd';
+import { RadioChangeEvent } from 'antd/lib/radio';
 
-const bitcoinIcon = require('@/assets/bitcoin.png');
+import styles from './payment.less';
+
+const stripeLogo = require('@/assets/stripe.png');
+const paypalLogo = require('@/assets/paypal.png');
+const coinbaseLogo = require('@/assets/coinbase.png');
 
 const { NODE_ENV } = process.env;
 
+const LIFETIME = 'LIFETIME';
+const SUBSCRIPTION = 'SUBSCRIPTION';
+
+// TODO: Move all the codes to some config (preferably provided from server or added in CI step)
+
+const paypalCodes = {
+  // first PRODUCTION, second DEVELOPMENT
+  [LIFETIME]: ['UBCLCJ384D2D4', '3FAV75HYMXJ5N'],
+  [SUBSCRIPTION]: ['PAJTMA62ZSBRW', 'WABX9M3L32NJS'],
+};
+
+const coinbaseCodes = ['5e8d6403-71dc-4988-8b06-f21d8d296cb3', '5e8d6403-71dc-4988-8b06-f21d8d296cb3'];
+
 const Payment: React.FC = () => {
-  const description =
-    'Logo; Unlock eaccess to 15 coil types calculator ; Sweet Spot Finder ; Advanced Coil specs ; Batteries charts; Min. battery resistance; Wires length; Visualize coils; Ad-free; Support project development';
+
+  const [ type, setType ] = React.useState(LIFETIME);
+  const [ step, setStep ] = React.useState(0);
+
+  const onChange = (e: RadioChangeEvent) => setType(e?.target?.value || LIFETIME);
+
+  const getPaypalHref = () => {
+    const code = paypalCodes[type][NODE_ENV === 'production' ? 0 : 1];
+    const sandBoxStr = NODE_ENV === 'development' ? 'sandbox.' : '';
+
+    return `https://www.${sandBoxStr}paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${code}`;
+  };
+
+  const getCoinbaseHref = () => {
+    const code = coinbaseCodes[NODE_ENV === 'production' ? 0 : 1];
+    return `https://commerce.coinbase.com/checkout/${code}`;
+  };
 
   return (
-    <Row>
-      <Col span={12}>
-        <Card>{description}</Card>
+    <Row gutter={16} type="flex" justify="center">
+      <Col span={12} lg={8}>
+        <Card className={styles.paymentCard}>
+          <Typography.Title>Vape Tool Pro</Typography.Title>
+          <Typography.Title level={4}>Choose you plan:</Typography.Title>
+          <Radio.Group onChange={onChange} value={type}>
+            <Radio value={LIFETIME} className={`${styles.paymentOption} ${type === LIFETIME ? styles.active : ''}`}>
+              <div className={styles.radioText}>
+                <Tag color="green">Best option</Tag>
+                <div>Lifetime @ $4.99</div>
+              </div>
+            </Radio>
+            <Radio value={SUBSCRIPTION} className={`${styles.paymentOption} ${type === SUBSCRIPTION ? styles.active : ''}`}>
+              <div className={styles.radioText}>
+                <Tag color="blue">Cheapest</Tag>
+                <div>Monthly @ $0.99/month</div>
+              </div>
+            </Radio>
+          </Radio.Group>
+          <Button type="primary" onClick={() => setStep(1)} disabled={step > 0} block>Continue</Button>
+
+          {step > 0 && (
+            <>
+              <Typography.Title level={4} style={{ marginTop: 24 }}>Choose you payment method:</Typography.Title>
+              <Row type="flex" justify="space-between" gutter={12} style={{ marginBottom: 24 }}>
+                <Col md={8}>
+                  <div className={`${styles.paymentMethod} ${styles.disabled}`}>
+                    <span className={styles.methodName}>Credit Card</span>
+                    <span className={styles.poweredBy}>powered by</span>
+                    <img src={stripeLogo} alt="Stripe" />
+                  </div>
+                </Col>
+                <Col md={8}>
+                  <a
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href={getPaypalHref()}
+                  >
+                    <div className={`${styles.paymentMethod} ${styles.paypalMethod}`}>
+                      <img src={paypalLogo} className={styles.paypalLogo} title="Pay with PayPal" alt="PayPal"/>
+                      <span className={styles.methodName}>checkout</span>
+                    </div>
+                  </a>
+                </Col>
+                <Col md={8}>
+                  <a
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    href={getCoinbaseHref()}
+                  >
+                    <div className={styles.paymentMethod}>
+                      <span className={styles.methodName}>Cryptocurrencies</span>
+                      <span className={styles.poweredBy}>powered by</span>
+                      <img src={coinbaseLogo} alt="Coinbase" />
+                    </div>
+                  </a>
+                </Col>
+              </Row>
+            </>
+          )}
+
+          <Typography.Paragraph className={styles.accepted}>
+            Credit cards (powered by Stripe), PayPal, and all major cryptocurrencies accepted.
+          </Typography.Paragraph>
+          <Typography.Paragraph className={styles.return} strong>
+            <Icon type="check-circle" theme="filled" />
+            Your purchase is fully refundable within 14 days.
+          </Typography.Paragraph>
+        </Card>
       </Col>
 
-      <Col span={12}>
-        <Card>
-          <Typography.Title>Vape Tool Pro</Typography.Title>
-          <Typography.Title>$4.99</Typography.Title>
-          <Typography.Paragraph>Lifetime</Typography.Paragraph>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href={`https://www.${
-              NODE_ENV === 'development' ? 'sandbox.' : ''
-            }paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${
-              NODE_ENV === 'production' ? 'UBCLCJ384D2D4' : '3FAV75HYMXJ5N'
-            }`}
-          >
-            <img
-              src="https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/44_Blue_CheckOut_Pill_Button.png"
-              alt="Check out with PayPal"
-            />
-          </a>
-
-          <Button
-            type="primary"
-            shape="round"
-            size="large"
-            target="_blank"
-            href="https://commerce.coinbase.com/checkout/5e8d6403-71dc-4988-8b06-f21d8d296cb3"
-          >
-            <img
-              style={{ maxWidth: 30, paddingRight: 8 }}
-              src={bitcoinIcon}
-              alt="cryptocurrency payment"
-            />
-            Pay with cryptocurrency
-          </Button>
-
-          <Typography.Title>$0.99</Typography.Title>
-          <Typography.Paragraph>Subscription</Typography.Paragraph>
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href={`https://www.${
-              NODE_ENV === 'development' ? 'sandbox.' : ''
-            }paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${
-              NODE_ENV === 'production' ? 'PAJTMA62ZSBRW' : 'WABX9M3L32NJS'
-            }`}
-          >
-            <img
-              src="https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/44_Blue_CheckOut_Pill_Button.png"
-              alt="Check out with PayPal"
-            />
-          </a>
-
-          <Button
-            type="primary"
-            shape="round"
-            size="large"
-            target="_blank"
-            href="https://commerce.coinbase.com/checkout/5e8d6403-71dc-4988-8b06-f21d8d296cb3"
-          >
-            <img
-              style={{ maxWidth: 30, paddingRight: 8 }}
-              src={bitcoinIcon}
-              alt="cryptocurrency payment"
-            />
-            Pay with cryptocurrency
-          </Button>
+      <Col md={12} lg={8}>
+        <Card className={styles.benefitsCard}>
+          <Typography.Title>Vape Tool Pro Benefits</Typography.Title>
+          <ul>
+            <li>Access to <b>15</b> coil types calculator</li>
+            <li>Sweet Spot Finder</li>
+            <li>Advanced Coil specs</li>
+            <li>Batteries charts</li>
+            <li>Min battery resistance</li>
+            <li>Wires length</li>
+            <li>Visualize coils</li>
+            <li>Ad-free</li>
+            <li>Support project development</li>
+          </ul>
         </Card>
       </Col>
     </Row>
