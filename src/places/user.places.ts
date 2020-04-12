fd132b9e773ab7174b9de455534cd427e39404a0
stasbar <stasbar1995@gmail.com>
2020-04-12 21:12:42 +0200

    all not prod REACT_APP_ENV should take dev flavor
    Took 1 hour 1 minute

--- src/places/user.places.ts
+++ src/places/user.places.ts
@@ -6,7 +6,7 @@ export const getUserLoginUrl = (): string => `/user/login`;
 
 export const getCancelSubscriptionUrl = (): string =>
   `https://www.${
-    REACT_APP_ENV === 'dev' ? 'sandbox.' : ''
+    REACT_APP_ENV !== 'prod' ? 'sandbox.' : ''
   }paypal.com/cgi-bin/webscr?cmd=_subscr-find&alias=${
-    REACT_APP_ENV === 'prod' ? 'ETUSF9JPSL3E8' : '62E6JFJB7ENUC'
+    REACT_APP_ENV !== 'prod' ? '62E6JFJB7ENUC' : 'ETUSF9JPSL3E8'
   }`;
