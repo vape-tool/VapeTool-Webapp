73d6a6068a3bc793eaa7bd656ee550861c286001
gbaranski <gbaranski19@gmail.com>
2020-06-04 01:45:08 +0200

    fetched data are now in proper place

--- src/pages/mixer/Mixer.tsx
+++ src/pages/mixer/Mixer.tsx
@@ -4,11 +4,11 @@ import { Button, Card, Col, Form, Row, Table, Typography } from 'antd';
 import ButtonGroup from 'antd/es/button/button-group';
 import { FormattedMessage } from 'umi-plugin-react/locale';
 import { UpOutlined } from '@ant-design/icons';
-import { MixableType, Mixable } from '@vapetool/types';
+import { MixableType, Mixable, MixResult } from '@vapetool/types';
 import InputElements from './inputElements';
 import SelectType from './SelectType';
 import { calculate } from '@/services/mixer';
-import { columns, data } from './tableData';
+import { columns } from './tableData';
 
 // TODO check if not needed adjust to new Form API
 const Mixer: React.FC = () => {
@@ -28,9 +28,23 @@ const Mixer: React.FC = () => {
     thinner: undefined,
     flavorPercentage: undefined,
   };
+
+  const mixResultPattern: Partial<MixResult> = {
+    amount: 0,
+    ratio: 0,
+    strength: 0,
+  };
+
   const [mixable1, setMixable1] = useState(mixDataPattern);
 
   const [mixable2, setMixable2] = useState(mixDataPattern);
+
+  const [results, setResults] = useState({
+    ...mixResultPattern,
+    results: [mixResultPattern, mixResultPattern, mixResultPattern],
+  });
+
+  const [resultsVisible, setResultsVisible] = useState(false);
 
   const handleClear = () => {
     setMixable1({
@@ -43,7 +57,11 @@ const Mixer: React.FC = () => {
 
   const handleCalculate = async (e: any) => {
     e.preventDefault();
-    console.log(await calculate(mixable1, mixable2));
+    await calculate(mixable1, mixable2).then(_results => {
+      setResultsVisible(true);
+      setResults(_results);
+      console.log(_results);
+    });
   };
 
   const formItemLayout = {
@@ -56,7 +74,32 @@ const Mixer: React.FC = () => {
       sm: { span: 16 },
     },
   };
-
+  const data = [
+    {
+      key: '1',
+      name: MixableType[mixable1.type],
+      percentage: Math.round(results.results[0].percentage * 100) / 100,
+      amount: results.results[0].ml,
+      drops: results.results[0].drips,
+      weight: results.results[0].weight,
+    },
+    {
+      key: '2',
+      name: MixableType[mixable2.type],
+      percentage: Math.round(results.results[1].percentage * 100) / 100,
+      amount: results.results[1].ml,
+      drops: results.results[1].drips,
+      weight: results.results[1].weight,
+    },
+    {
+      key: '3',
+      name: 'Total',
+      percentage: 100,
+      amount: results.results[1].ml + results.results[2].ml,
+      drops: results.results[1].drips + results.results[2].drips,
+      weight: results.results[1].weight + results.results[2].weight,
+    },
+  ];
   return (
     <PageHeaderWrapper>
       <Card>
@@ -86,35 +129,37 @@ const Mixer: React.FC = () => {
                 </Col>
               </Row>
 
-              <Card>
-                <Row
-                  style={{
-                    textAlign: 'center',
-                    alignItems: 'center',
-                    flex: 1,
-                    flexDirection: 'row',
-                    fontSize: 24,
-                  }}
-                >
-                  <Col style={{ margin: 'auto' }}>
-                    <Row>
-                      <Typography style={{ fontWeight: 'bold', fontSize: 24 }}>
-                        Ratio:&nbsp;
-                      </Typography>
-                      <Typography>55VG/45PG</Typography>
-                    </Row>
-                  </Col>
-                  <Col style={{ margin: 'auto' }}>
-                    <Row>
-                      <Typography style={{ fontWeight: 'bold', fontSize: 24 }}>
-                        Strength:&nbsp;
-                      </Typography>
-                      <Typography>18.00 mg/ml</Typography>
-                    </Row>
-                  </Col>
-                </Row>
-                <Table columns={columns} dataSource={data} pagination={false} />
-              </Card>
+              {resultsVisible && (
+                <Card>
+                  <Row
+                    style={{
+                      textAlign: 'center',
+                      alignItems: 'center',
+                      flex: 1,
+                      flexDirection: 'row',
+                      fontSize: 24,
+                    }}
+                  >
+                    <Col style={{ margin: 'auto' }}>
+                      <Row>
+                        <Typography style={{ fontWeight: 'bold', fontSize: 24 }}>
+                          Ratio:&nbsp;
+                        </Typography>
+                        <Typography>55VG/45PG</Typography>
+                      </Row>
+                    </Col>
+                    <Col style={{ margin: 'auto' }}>
+                      <Row>
+                        <Typography style={{ fontWeight: 'bold', fontSize: 24 }}>
+                          Strength:&nbsp;
+                        </Typography>
+                        <Typography>{results.strength}mg/ml</Typography>
+                      </Row>
+                    </Col>
+                  </Row>
+                  <Table columns={columns} dataSource={data} pagination={false} />
+                </Card>
+              )}
 
               <Col>
                 <Form.Item style={{ marginTop: 20, display: 'flex', marginLeft: 'auto' }}>
