6290a3ba84477c20a7ea3502a6b0ef128b5891fd
gbaranski <gbaranski19@gmail.com>
2020-06-11 23:32:51 +0200

    Same as previous, but now also for liquid

--- src/components/saveToDatabaseModal/saveLiquidModal.tsx
+++ src/components/saveToDatabaseModal/saveLiquidModal.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
-import { Modal } from 'antd';
+import { Modal, Input } from 'antd';
 import { saveLiquid } from '@/services/items';
 import { Author } from '@vapetool/types';
+import FormItem from 'antd/lib/form/FormItem';
+import { FormattedMessage } from 'umi-plugin-react/locale';
 
 export default function SaveLiquidModal(props: any) {
   const [isLoading, setLoading] = useState(false);
+  const [liquidName, setLiquidName] = useState('');
+  const [liquidDescription, setLiquidDescription] = useState('');
+
   const handleOk = async () => {
     setLoading(true);
-    await saveLiquid(props.currentLiquid, new Author(props.userUid, props.userName), '', '');
+    await saveLiquid(
+      props.currentLiquid,
+      new Author(props.userUid, props.userName),
+      liquidName,
+      liquidDescription,
+    );
     setTimeout(() => {
       setLoading(false);
       props.setVisible(false);
     }, 200);
   };
-  const handleCancel = async () => {};
+  const handleCancel = () => {
+    props.setVisible(false);
+  };
   return (
     <Modal
       title="Title"
@@ -22,7 +34,22 @@ export default function SaveLiquidModal(props: any) {
       confirmLoading={isLoading}
       onCancel={handleCancel}
     >
-      <p>Are you sure to save liquid?</p>
+      <FormItem label={<FormattedMessage id="liquid.name" defaultMessage="Liquid name" />}>
+        <Input
+          placeholder="Liquid name"
+          value={liquidName}
+          onChange={e => setLiquidName(e.target.value)}
+        />
+      </FormItem>
+      <FormItem
+        label={<FormattedMessage id="liquid.description" defaultMessage="Liquid description" />}
+      >
+        <Input
+          placeholder="Liquid description"
+          value={liquidDescription}
+          onChange={e => setLiquidDescription(e.target.value)}
+        />
+      </FormItem>
     </Modal>
   );
 }
