fb97f27fe87495571a92d5b0dd01adbeb3cc9d81
gbaranski <gbaranski19@gmail.com>
2020-06-11 23:24:37 +0200

    Add name and description input

--- src/components/saveToDatabaseModal/saveCoilModal.tsx
+++ src/components/saveToDatabaseModal/saveCoilModal.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import { Modal } from 'antd';
+import { Modal, Input, Form } from 'antd';
 import { saveCoil } from '@/services/items';
 import { Author } from '@vapetool/types';
+import FormItem from 'antd/lib/form/FormItem';
+import { FormattedMessage } from 'umi-plugin-react/locale';
 
 export default function SaveCoilModal(props: any) {
   const [isLoading, setLoading] = useState(false);
+  const [coilName, setCoilName] = useState('');
+  const [coilDescription, setCoilDescription] = useState('');
+
   const handleOk = async () => {
     setLoading(true);
-    await saveCoil(props.coil, new Author(props.userUid, props.userName), '', '');
+    await saveCoil(
+      props.coil,
+      new Author(props.userUid, props.userName),
+      coilName,
+      coilDescription,
+    );
     setTimeout(() => {
       setLoading(false);
       props.setVisible(false);
@@ -22,7 +32,24 @@ export default function SaveCoilModal(props: any) {
       confirmLoading={isLoading}
       onCancel={handleCancel}
     >
-      <p>Are you sure to save coil?</p>
+      <Form onSubmitCapture={e => e.preventDefault()}>
+        <FormItem label={<FormattedMessage id="coil.name" defaultMessage="Coil name" />}>
+          <Input
+            placeholder="Coil name"
+            value={coilName}
+            onChange={e => setCoilName(e.target.value)}
+          />
+        </FormItem>
+      </Form>
+      <FormItem
+        label={<FormattedMessage id="coil.description" defaultMessage="Coil description" />}
+      >
+        <Input
+          placeholder="Coil description"
+          value={coilDescription}
+          onChange={e => setCoilDescription(e.target.value)}
+        />
+      </FormItem>
     </Modal>
   );
 }
