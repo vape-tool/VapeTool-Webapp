a4e7f9b3b12bc59a3aa07ff0779749fd70306559
gbaranski <gbaranski19@gmail.com>
2020-07-11 19:38:31 +0200

    fix upload model

--- src/models/upload.ts
+++ src/models/upload.ts
@@ -1,45 +1,15 @@
-import { Dispatch, Reducer } from 'umi';
+import { useState } from 'react';
 
 export enum Tab {
   PHOTO = 'photo',
   POST = 'post',
   LINK = 'link',
 }
-export const UPLOAD = 'upload';
-export const SET_TAB = 'setTab';
+export default function() {
+  const [currentTab, setCurrentTab] = useState<Tab>(Tab.PHOTO);
 
-export const changeTab = (dispatch: Dispatch) => (tab: string) => {
-  dispatch({
-    type: `${UPLOAD}/${SET_TAB}`,
-    tab,
-  });
-};
-
-export interface UploadState {
-  currentTab: 'post' | 'photo' | 'link';
-}
-
-interface ModelType {
-  namespace: string;
-  state: UploadState;
-  reducers: {
-    [SET_TAB]: Reducer<UploadState>;
+  return {
+    setTab: setCurrentTab,
+    currentTab,
   };
 }
-
-const Model: ModelType = {
-  namespace: UPLOAD,
-
-  state: { currentTab: 'photo' },
-
-  reducers: {
-    setTab(state, { tab }) {
-      return {
-        ...state,
-        currentTab: tab,
-      };
-    },
-  },
-};
-
-export default Model;
