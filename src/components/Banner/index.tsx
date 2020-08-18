89f065e87d9cfe67904934e3afd69a93b5e902e8
gbaranski <gbaranski19@gmail.com>
2020-08-18 04:30:34 +0200

    add functionality to check if user is pro or anonymous

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { getAdImageProperties, BannerProperties, getBannerUrl } from '@/services/storage';
+import { useModel } from 'umi';
+import { isProUser } from '@/utils/utils';
 
 export default (props: { providerName: string }) => {
+  const { initialState } = useModel('@@initialState');
+  const { firebaseUser, currentUser } = initialState || {};
+
   const [bannerProperties, setBannerProperties] = useState<BannerProperties>();
   const [bannerSrc, setBannerSrc] = useState<string | undefined>();
+  if (firebaseUser?.isAnonymous || isProUser(currentUser?.subscription)) {
+    return <></>;
+  }
+
   useEffect(() => {
     getAdImageProperties(props.providerName).then((_adProperties) => {
       console.log({ _adProperties });
